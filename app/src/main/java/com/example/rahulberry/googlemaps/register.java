8fc7f0cd65e667c611b75eff59b37fd7fe73d8ad
Rahul Berry <rahul_berry@hotmail.co.uk>
2018-02-13 15:24:18 +0000

    Fixed Constraints

--- app/src/main/java/com/example/rahulberry/googlemaps/register.java
+++ app/src/main/java/com/example/rahulberry/googlemaps/register.java
@@ -1,13 +1,27 @@
 package com.example.rahulberry.googlemaps;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
 
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_register);
+
+        register = (Button)findViewById(R.id.register_2);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(register.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
